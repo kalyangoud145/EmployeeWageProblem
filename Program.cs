44abccc4ae2e3ad1ad6a71d5e49b9d59efc0d867
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-21 13:17:37 +0530

    [kalyan] added daily wage calculation functionality

--- Program.cs
+++ Program.cs
@@ -7,11 +7,12 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             const int IS_EMPLOYEE_FULL_TIME = 1;
+            const int WORKING_HRS = 8, WAGE_PER_HR = 20;
             Random random = new Random();
             int empCheck = random.Next(2);
             if( empCheck == IS_EMPLOYEE_FULL_TIME)
             {
-                Console.WriteLine("Employee Present");
+                Console.WriteLine("Employee present and daily wage: " + WAGE_PER_HR * WORKING_HRS);
             }
             else
             {
