2a2abb22d84390e4e1798c0192516e22a2a50066
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-21 14:47:36 +0530

    [kalyan] added condition for employee monthly wage

--- Program.cs
+++ Program.cs
@@ -6,25 +6,37 @@ namespace EmployeeWageProblem
     {
         static void Main(string[] args)
         {
+            //constants 
             const int IS_EMPLOYEE_FULL_TIME = 1, IS_EMPLOYEE_PART_TIME = 2;
-            const int FULL_TIME_WORKING_HRS = 8, WAGE_PER_HR = 20;
-            const int PART_TIME_WORKING_HRS = 4;
+            const int WAGE_PER_HR = 20;
+            const int MAX_WORKING_DAYS = 20;
 
             Random random = new Random();
-            int empCheck = random.Next(3);
+            int empHrs = 0, totalWorkingDays = 0;
+            int totalWorkingHrs = 0;
 
-            switch (empCheck)
+            //While loop for checking the total employee working days 
+            while (totalWorkingDays <= MAX_WORKING_DAYS)
             {
-                case IS_EMPLOYEE_FULL_TIME:
-                    Console.WriteLine("Employee fulltime and daily wage: " + WAGE_PER_HR * FULL_TIME_WORKING_HRS);
-                    break;
-                case IS_EMPLOYEE_PART_TIME:
-                    Console.WriteLine("Employee Parttime and daily wage: " + WAGE_PER_HR * PART_TIME_WORKING_HRS);
-                    break;
-                default:
-                    Console.WriteLine("Employee absent");
-                    break;
+                int empCheck = random.Next(3);
+                switch (empCheck)
+                {
+                    case IS_EMPLOYEE_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_EMPLOYEE_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalWorkingDays++;
+                totalWorkingHrs += empHrs;
+
             }
+            ///Printing the total employee wage for 20 days
+            Console.WriteLine("Employee Monthly wage: " + (totalWorkingHrs * WAGE_PER_HR));
         }
     }
 }
