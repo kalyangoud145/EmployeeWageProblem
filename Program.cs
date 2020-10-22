8a00b882f8c0fc8adedd53d86fb3123d2f80563e
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-22 10:12:49 +0530

    [Kalyan] refactored code to use classes and objects

--- Program.cs
+++ Program.cs
@@ -8,10 +8,15 @@ namespace EmployeeWageProblem
         public const int IS_EMPLOYEE_FULL_TIME = 1, IS_EMPLOYEE_PART_TIME = 2;
         public const int WAGE_PER_HR = 20, MAX_WORKING_HRS = 100;
         public const int MAX_WORKING_DAYS = 20;
+
         static void Main(string[] args)
         {
-            
+            ///Printing the total employee wage for 20 days and maximum working hrs are 100
+            Console.WriteLine("Employee Monthly wage: " + CalculateEmployeeWage());
+        }
 
+        public static int CalculateEmployeeWage()
+        {
             Random random = new Random();
             int empHrs = 0, totalWorkingDays = 0;
             int totalWorkingHrs = 0;
@@ -36,8 +41,8 @@ namespace EmployeeWageProblem
                 totalWorkingHrs += empHrs;
 
             }
-            ///Printing the total employee wage for 20 days and maximum working hrs are 100
-            Console.WriteLine("Employee Monthly wage: " + (totalWorkingHrs * WAGE_PER_HR));
+            ///Returns the employee monthly wage
+            return (totalWorkingHrs * WAGE_PER_HR);
         }
     }
 }
