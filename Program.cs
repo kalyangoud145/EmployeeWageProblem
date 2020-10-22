bdbfa77a59a08e825a8221a07526cdbebdd7966f
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-22 11:51:56 +0530

    [Kalyan] refactored to calculate wage for multiple companies

--- Program.cs
+++ Program.cs
@@ -6,16 +6,16 @@ namespace EmployeeWageProblem
     {
         //constants 
         public const int IS_EMPLOYEE_FULL_TIME = 1, IS_EMPLOYEE_PART_TIME = 2;
-        public const int WAGE_PER_HR = 20, MAX_WORKING_HRS = 100;
-        public const int MAX_WORKING_DAYS = 20;
+   
 
         static void Main(string[] args)
         {
-            ///Printing the total employee wage for 20 days and maximum working hrs are 100
-            Console.WriteLine("Employee Monthly wage: " + CalculateEmployeeWage());
+            ///Printing the total employee wage for differnt companies
+            Console.WriteLine("Employee Monthly wage: " + CalculateEmployeeWage("Microsoft",50,24,60));
+            Console.WriteLine("Employee Monthly wage: " + CalculateEmployeeWage("Google", 90, 25, 70));
         }
 
-        public static int CalculateEmployeeWage()
+        public static int CalculateEmployeeWage(string companyname, int WAGE_PER_HR, int MAX_WORKING_DAYS, int MAX_WORKING_HRS)
         {
             Random random = new Random();
             int empHrs = 0, totalWorkingDays = 0;
