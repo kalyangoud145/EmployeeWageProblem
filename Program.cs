1920f17166c7acdaf914ebff6d4e6a3f58f0e408
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-22 09:25:28 +0530

    [Kalyan] changed the scope of constant variables

--- Program.cs
+++ Program.cs
@@ -4,12 +4,13 @@ namespace EmployeeWageProblem
 {
     class Program
     {
+        //constants 
+        const int IS_EMPLOYEE_FULL_TIME = 1, IS_EMPLOYEE_PART_TIME = 2;
+        const int WAGE_PER_HR = 20, MAX_WORKING_HRS = 100;
+        const int MAX_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
-            //constants 
-            const int IS_EMPLOYEE_FULL_TIME = 1, IS_EMPLOYEE_PART_TIME = 2;
-            const int WAGE_PER_HR = 20, MAX_WORKING_HRS = 100;
-            const int MAX_WORKING_DAYS = 20;
+            
 
             Random random = new Random();
             int empHrs = 0, totalWorkingDays = 0;
