14235112e75f34db2a9ccc60d0a8e336a069f6e6
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-23 09:45:54 +0530

    [Kalyan] refactored code to class and object type

--- Program.cs
+++ Program.cs
@@ -4,45 +4,22 @@ namespace EmployeeWageProblem
 {
     class Program
     {
-        //constants 
-        public const int IS_EMPLOYEE_FULL_TIME = 1, IS_EMPLOYEE_PART_TIME = 2;
-   
 
+
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            ///Printing the total employee wage for differnt companies
-            Console.WriteLine("Employee Monthly wage: " + CalculateEmployeeWage("Microsoft",50,24,60));
-            Console.WriteLine("Employee Monthly wage: " + CalculateEmployeeWage("Google", 90, 25, 70));
+            EmployeeBuilderObject microsoft = new EmployeeBuilderObject("Microsoft", 50, 24, 60);
+            EmployeeBuilderObject google = new EmployeeBuilderObject("Google", 90, 25, 70);
+            microsoft.CalculateEmployeeWage();
+            Console.WriteLine(microsoft.PrintTotalEmpWage());
+            google.CalculateEmployeeWage();
+            Console.WriteLine(google.PrintTotalEmpWage());
         }
 
-        public static int CalculateEmployeeWage(string companyname, int WAGE_PER_HR, int MAX_WORKING_DAYS, int MAX_WORKING_HRS)
-        {
-            Random random = new Random();
-            int empHrs = 0, totalWorkingDays = 0;
-            int totalWorkingHrs = 0;
 
-            //While loop for checking the total employee working days 
-            while (totalWorkingHrs <= MAX_WORKING_HRS && totalWorkingDays <= MAX_WORKING_DAYS)
-            {
-                int empCheck = random.Next(3);
-                switch (empCheck)
-                {
-                    case IS_EMPLOYEE_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case IS_EMPLOYEE_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalWorkingDays++;
-                totalWorkingHrs += empHrs;
-
-            }
-            ///Returns the employee monthly wage
-            return (totalWorkingHrs * WAGE_PER_HR);
-        }
     }
 }
